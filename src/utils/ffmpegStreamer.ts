78712c351feb45f2d948eec790590b44e4f48b6f
bluenk <ian8896877@gmail.com>
2023-12-05 17:05:39 +0800

    feat: support gif making ref: #31

--- src/utils/ffmpegStreamer.ts
+++ src/utils/ffmpegStreamer.ts
@@ -7,18 +7,28 @@ const ffmpegPath = '/usr/bin/ffmpeg';
 /**
  * Convert m3u8 url to stream.
  * @param url m3u8 url
+ * @param mode select working mode
  */
 
-export default function ffmpegStreamer(url: string) {
-    log('Start streaming data.');
+export default function ffmpegStreamer(url: string, mode: 'STREAM_MP4' | 'GIF') {
+    log('Start streaming data...');
+    log('Streaming mode: ' + mode);
 
-    const ffmpeg = spawn(ffmpegPath, [
+    const gifOptions = [
+        '-i', url,
+        '-vf', 'split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse',
+        '-f', 'GIF',
+        '-'
+    ];
+    const mp4Options = [
         '-i', url,
         '-c', 'copy',
         '-f', 'mp4',
         '-movflags', 'frag_keyframe+empty_moov',
         '-'
-    ]);
+    ];
+
+    const ffmpeg = spawn(ffmpegPath, mode === 'STREAM_MP4' ? mp4Options : gifOptions);
 
     ffmpeg.stderr.on('error', console.error);
 
